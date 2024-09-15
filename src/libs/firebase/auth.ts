69d2024cd83e1a1e02b1d1894c6fde914fcd3e29
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2024-09-14 21:32:18 -0600

    feat: 🎸 Updated signIn function to save user data

--- src/libs/firebase/auth.ts
+++ src/libs/firebase/auth.ts
@@ -5,7 +5,8 @@ import {
   onAuthStateChanged as _onAuthStateChanged,
 } from "firebase/auth";
 
-import { firebaseAuth } from "./config";
+import { db, firebaseAuth } from "./config";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 export function onAuthStateChanged(callback: (authUser: User | null) => void) {
   return _onAuthStateChanged(firebaseAuth, callback);
@@ -20,6 +21,32 @@ export async function signInWithGoogle() {
     if (!result || !result.user) {
       throw new Error("Google sign in failed");
     }
+
+    const docSnap = await getDoc(doc(db, "users", result.user.uid));
+    if (!docSnap.exists()) {
+      await setDoc(doc(db, "users", result.user.uid), {
+        uid: result.user.uid,
+        email: result.user.email,
+        displayName: result.user.displayName,
+        photoURL: result.user.photoURL,
+      });
+
+      alert("User added to firestore");
+
+      await fetch(`${process.env.BACKEND_URL}/add_user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: result.user.displayName,
+          email: result.user.email,
+        }),
+      });
+
+      alert("User added to backend");
+    }
+
     return result.user.uid;
   } catch (error) {
     console.error("Error signing in with Google", error);
